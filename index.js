5c69764e135a50671eb1147937cbc8733fd59a1f
Abena Amo <abenaserwaaamo@gmail.com>
2024-12-05 19:19:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
